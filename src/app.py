0109e7c75520346e3000ecf63d8f24aa3071586b
Tomaz Bratanic <bratanic.tomaz@gmail.com>
2023-09-14 19:10:47 +0200

    froented api key

--- src/app.py
+++ src/app.py
@@ -14,17 +14,11 @@ from cypher_chain import CustomCypherChain
 url = st.secrets["NEO4J_URL"]
 username = st.secrets["NEO4J_USERNAME"]
 password = st.secrets["NEO4J_PASSWORD"]
-openai_api_key = st.secrets["OPENAI_API_KEY"]
-os.environ["OPENAI_API_KEY"] = openai_api_key
 
 # Langchain x Neo4j connections
 graph = Neo4jGraph(username=username, password=password, url=url)
 
-graph_search = CustomCypherChain.from_llm(
-    cypher_llm=ChatOpenAI(temperature=0.7, model_name="gpt-4"),
-    qa_llm=ChatOpenAI(temperature=0.7),
-    graph=graph,
-)
+graph_search = None
 
 # Session state
 if "generated" not in st.session_state:
@@ -60,9 +54,21 @@ def generate_context(
 
 
 def get_text() -> str:
-    input_text = st.chat_input("Ask your question")
-    return input_text
+    input_text = st.chat_input("Who is the CEO of Neo4j?")
+    if not openai_api_key.startswith('sk-'):
+        st.warning('Please enter your OpenAI API key!', icon='⚠')
+    else:
+        return input_text
+    
 
+openai_api_key = st.sidebar.text_input('OpenAI API Key')
+os.environ["OPENAI_API_KEY"] = openai_api_key
+if openai_api_key:
+    graph_search = CustomCypherChain.from_llm(
+    cypher_llm=ChatOpenAI(temperature=0.7, model_name="gpt-4"),
+    qa_llm=ChatOpenAI(temperature=0.7),
+    graph=graph,
+)
 
 user_input = get_text()
 
