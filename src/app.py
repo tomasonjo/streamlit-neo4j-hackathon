eea03ba5de6da9e2e30beabcc303c9f26e95b807
Tomaz Bratanic <bratanic.tomaz@gmail.com>
2023-09-14 09:29:08 +0200

    frontend

--- src/app.py
+++ src/app.py
@@ -1,10 +1,13 @@
 import os
+from typing import List, Union
+
 import streamlit as st
+import graphviz
 from langchain.chat_models import ChatOpenAI
 from langchain.graphs import Neo4jGraph
 from langchain.schema import HumanMessage, AIMessage
 
-st.title("Knowledge Graph Chatbot")
+st.title("VC Chatbot")
 
 from cypher_chain import CustomCypherChain
 
@@ -12,15 +15,15 @@ url = st.secrets["NEO4J_URL"]
 username = st.secrets["NEO4J_USERNAME"]
 password = st.secrets["NEO4J_PASSWORD"]
 openai_api_key = st.secrets["OPENAI_API_KEY"]
-os.environ['OPENAI_API_KEY'] = openai_api_key
+os.environ["OPENAI_API_KEY"] = openai_api_key
 
 # Langchain x Neo4j connections
 graph = Neo4jGraph(username=username, password=password, url=url)
 
 graph_search = CustomCypherChain.from_llm(
-    cypher_llm=ChatOpenAI(temperature=0.7, model_name='gpt-4'),
+    cypher_llm=ChatOpenAI(temperature=0.7, model_name="gpt-4"),
     qa_llm=ChatOpenAI(temperature=0.7),
-    graph=graph
+    graph=graph,
 )
 
 # Session state
@@ -30,40 +33,70 @@ if "generated" not in st.session_state:
 if "user_input" not in st.session_state:
     st.session_state["user_input"] = []
 
-def generate_context(prompt, context_data='generated'):
+if "viz_data" not in st.session_state:
+    st.session_state["viz_data"] = []
+
+if "database" not in st.session_state:
+    st.session_state["database"] = []
+
+if "cypher" not in st.session_state:
+    st.session_state["cypher"] = []
+
+
+def generate_context(
+    prompt: str, context_data: str = "generated"
+) -> List[Union[AIMessage, HumanMessage]]:
     context = []
     # If any history exists
-    if st.session_state['generated']:
+    if st.session_state["generated"]:
         # Add the last three exchanges
-        size = len(st.session_state['generated'])
-        for i in range(max(size-3, 0), size):
-            context.append(
-                HumanMessage(content=st.session_state['user_input'][i]))
-            context.append(
-                AIMessage(content=st.session_state[context_data][i]))
+        size = len(st.session_state["generated"])
+        for i in range(max(size - 3, 0), size):
+            context.append(HumanMessage(content=st.session_state["user_input"][i]))
+            context.append(AIMessage(content=st.session_state[context_data][i]))
     # Add the latest user prompt
     context.append(HumanMessage(content=str(prompt)))
     return context
 
 
-def get_text():
+def get_text() -> str:
     input_text = st.chat_input("Ask your question")
     return input_text
+
 
 user_input = get_text()
 
 if user_input:
     context = generate_context(user_input)
-    output = graph_search.run(query=user_input, chat_history=context)
+    output = graph_search({"query": user_input, "chat_history": context})
 
     st.session_state.user_input.append(user_input)
-    st.session_state.generated.append(output)
+    st.session_state.generated.append(output["result"])
+    st.session_state.viz_data.append(output["viz_data"])
+    st.session_state.database.append(output["database"])
+    st.session_state.cypher.append(output["cypher"])
 
-if st.session_state['generated']:
-    size = len(st.session_state['generated'])
+if st.session_state["generated"]:
+    size = len(st.session_state["generated"])
     # Display only the last three exchanges
-    for i in range(max(size-3, 0), size):
+    for i in range(max(size - 3, 0), size):
         with st.chat_message("user"):
-            st.write(st.session_state['user_input'][i])
+            st.write(st.session_state["user_input"][i])
         with st.chat_message("assistant"):
-            st.write(st.session_state["generated"][i])
+            chat, visualization, database, cypher = st.tabs(
+                ["Chat", "Visualization", "Database results", "Cypher"]
+            )
+            with chat:
+                st.write(st.session_state["generated"][i])
+            with visualization:
+                if st.session_state["viz_data"][i]:
+                    graph_object = graphviz.Digraph()
+                    for record in st.session_state["viz_data"][i]:
+                        graph_object.edge(
+                            record["source"], record["target"], label=record["type"]
+                        )
+                    st.graphviz_chart(graph_object)
+            with database:
+                st.write(st.session_state["database"][i])
+            with cypher:
+                st.code(st.session_state["cypher"][i], language="cypher")
--- src/cypher_chain.py
+++ src/cypher_chain.py
@@ -1,7 +1,6 @@
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Tuple
 from langchain.chains.graph_qa.cypher import extract_cypher
 from langchain.schema import SystemMessage
-
 
 
 import spacy
@@ -10,6 +9,7 @@ from langchain.callbacks.manager import CallbackManagerForChainRun
 
 
 from cypher_validator import CypherValidator
+
 
 def remove_entities(doc):
     """
@@ -33,7 +33,7 @@ def remove_entities(doc):
     # Iterate through entities, replacing them with their entity label
     for ent in doc.ents:
         # Add the tokens that come before this entity
-        new_tokens.extend([token.text for token in doc[last_end:ent.start]])
+        new_tokens.extend([token.text for token in doc[last_end : ent.start]])
         # Replace the entity with its label
         new_tokens.append(f"{ent.label_}")
         # Update the last entity end index
@@ -44,6 +44,14 @@ def remove_entities(doc):
     # Join the new tokens into a single string
     new_text = " ".join(new_tokens)
     return new_text
+
+
+def process_entities(text: str) -> Tuple[List[str], str]:
+    spacy_doc = nlp(text)
+    entities = [ent.text for ent in spacy_doc.ents if ent.label_ in ["PERSON", "ORG"]]
+    cleaned_question = remove_entities(spacy_doc)
+
+    return entities, cleaned_question
 
 
 AVAILABLE_RELATIONSHIPS = """
@@ -72,21 +80,54 @@ Do not use any other relationship types or properties that are not provided.
 nlp = spacy.load("en_core_web_md")
 validator = CypherValidator()
 
+
 class CustomCypherChain(GraphCypherQAChain):
-    def find_entity_match(self, entity:str, k: int = 3):
+    def get_viz_data(self, entities: List[str]) -> List[Tuple[str, str]]:
+        viz_query = """
+        MATCH (n:Person|Organization) WHERE n.name IN $entities
+        CALL {
+            WITH n
+            MATCH (n)-[r:!MENTIONS]->(m)
+            WHERE m.name IS NOT NULL
+            RETURN n.name AS source, type(r) AS type, m.name AS target
+            LIMIT 5
+            UNION
+            WITH n
+            MATCH (n)<-[r:!MENTIONS]-(m)
+            WHERE m.name IS NOT NULL
+            RETURN n.name AS target, type(r) AS type, m.name AS source
+            LIMIT 5
+        }
+        RETURN source, type, target LIMIT 20
+        """
+        results = self.graph.query(viz_query, {"entities": entities})
+        return results
+
+    def find_entity_match(self, entity: str, k: int = 3) -> List[str]:
         fts_query = """
         CALL db.index.fulltext.queryNodes('entity', $entity + "*", {limit:$k})
         YIELD node,score
         RETURN node.name AS result
         """
 
-        return [el['result'] for el in self.graph.query(fts_query, {'entity': "AND ".join(entity.split()), 'k':k})]
-    
-    def generate_system_message(self, relevant_entities: str = "", fewshot_examples: str = ""):
+        return [
+            el["result"]
+            for el in self.graph.query(
+                fts_query, {"entity": "AND ".join(entity.split()), "k": k}
+            )
+        ]
+
+    def generate_system_message(
+        self, relevant_entities: str = "", fewshot_examples: str = ""
+    ) -> SystemMessage:
         system_message = CYPHER_SYSTEM_TEMPLATE
-        system_message += f"The database has the following schema: {self.graph.get_schema} "
+        system_message += (
+            f"The database has the following schema: {self.graph.get_schema} "
+        )
         if relevant_entities:
-            system_message += f"Relevant entities for the question are: {relevant_entities} "
+            system_message += (
+                f"Relevant entities for the question are: {relevant_entities} "
+            )
         if fewshot_examples:
             system_message += f"Follow these Cypher examples when you are constructing a Cypher statement: {fewshot_examples} "
         return SystemMessage(content=system_message)
@@ -99,39 +140,49 @@ class CustomCypherChain(GraphCypherQAChain):
 
         _run_manager = run_manager or CallbackManagerForChainRun.get_noop_manager()
         callbacks = _run_manager.get_child()
-        print(inputs)
         question = inputs[self.input_key]
-        chat_history = inputs['chat_history']
+        chat_history = inputs["chat_history"]
         intermediate_steps: List = []
-
-
-        spacy_doc = nlp(question)
-
         # Extract mentioned people and organizations and match them to database values
-        entities = [{'text':ent.text, 'label': ent.label_} for ent in spacy_doc.ents if ent.label_ in ["PERSON", "ORG"]]
+        entities, clean_question = process_entities(question)
         print(f"SpaCy found: {entities}")
         relevant_entities = dict()
         for entity in entities:
-            relevant_entities[entity['text']] = self.find_entity_match(entity['text'])
+            relevant_entities[entity] = self.find_entity_match(entity)
         print(f"Relevant entities are: {relevant_entities}")
 
         # Get few-shot examples using vector search
-        cleaned_question = remove_entities(spacy_doc)
+
         fewshots = "vectorsearch"
 
         system = self.generate_system_message(str(relevant_entities))
-        generated_cypher = self.cypher_generation_chain.llm.predict_messages([system] + chat_history)
+        generated_cypher = self.cypher_generation_chain.llm.predict_messages(
+            [system] + chat_history
+        )
         print(generated_cypher.content)
         generated_cypher = extract_cypher(generated_cypher.content)
-        validated_cypher = validator.validate_query(AVAILABLE_RELATIONSHIPS, generated_cypher)
+        validated_cypher = validator.validate_query(
+            AVAILABLE_RELATIONSHIPS, generated_cypher
+        )
         print(validated_cypher)
         # Retrieve and limit the number of results
         context = self.graph.query(validated_cypher[0])[: self.top_k]
 
         result = self.qa_chain(
-                {"question": question, "context": context},
-                callbacks=callbacks,
-            )
+            {"question": question, "context": context}, callbacks=callbacks
+        )
         final_result = result[self.qa_chain.output_key]
-        chain_result: Dict[str, Any] = {self.output_key: final_result}
+
+        final_entities, _ = process_entities(final_result)
+        if final_entities:
+            viz_data = self.get_viz_data(final_entities)
+        else:
+            viz_data = None
+
+        chain_result: Dict[str, Any] = {
+            self.output_key: final_result,
+            "viz_data": viz_data,
+            "database": context,
+            "cypher": validated_cypher[0],
+        }
         return chain_result
